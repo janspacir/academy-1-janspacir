c1af1c492b40ef1b5f42da2f15248b19ca51c47b
janspacir <jan.spacir@gmail.com>
2020-10-23 19:35:36 +0200

    Update component.py

--- src/component.py
+++ src/component.py
@@ -72,7 +72,7 @@ class Component(KBCEnvHandler):
                 primary_key=['row_number'],
                 incremental=True,
                 columns=new_columns)
-        with CachedOrthogonalDictWriter(result_file_path=os.path.join(self.tables_out_path, 'output.csv'), new_columns) as writer:
+        with CachedOrthogonalDictWriter(result_file_path=os.path.join(self.tables_out_path, 'output.csv')) as writer:
                 for index, l in enumerate(reader):
                     # print line
                     if params.get(KEY_PRINT_ROWS):
